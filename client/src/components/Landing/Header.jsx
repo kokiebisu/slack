c16ae18160a1c88b74aab2682c8f934a4b43188c
Ken <a01056715@gmail.com>
2020-05-03 00:06:53 -0700

    added dropdown

--- client/src/components/Landing/Header.jsx
+++ client/src/components/Landing/Header.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 // Blocks
 import { Box, Container, Flex, Text, device } from '../../styles/Blocks';
@@ -10,6 +11,14 @@ import { BottomArrow } from '../../assets/svg/Arrows';
 import { HamburgerButton } from '../../assets/svg/Buttons';
 
 export const Header = () => {
+  const [hovered, setHovered] = useState({
+    why: false,
+    solutions: false,
+    resources: false,
+    enterprise: false,
+    pricing: false,
+  });
+
   return (
     <Wrapper>
       <Container>
@@ -22,13 +31,117 @@ export const Header = () => {
           <Right justifyContent='space-between' alignItems='center'>
             <Nav>
               <NavItem>
-                <Text>Why Slack?</Text>
+                <motion.div
+                  onHoverStart={() => setHovered({ ...hovered, why: true })}
+                  onHoverEnd={() => setHovered({ ...hovered, why: false })}>
+                  <Text>Why Slack?</Text>
+                </motion.div>
+                {hovered.why ? (
+                  <Dropdown>
+                    <LinkWrapper>
+                      <Link>Features</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>How it replaces email</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Security</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Customers</Link>
+                    </LinkWrapper>
+                  </Dropdown>
+                ) : null}
               </NavItem>
               <NavItem>
-                <Text>Solutions</Text>
+                <motion.div
+                  onHoverStart={() =>
+                    setHovered({ ...hovered, solutions: true })
+                  }
+                  onHoverEnd={() =>
+                    setHovered({ ...hovered, solutions: false })
+                  }>
+                  <Text>Solutions</Text>
+                </motion.div>
+                {hovered.solutions ? (
+                  <Dropdown>
+                    <LinkWrapper>
+                      <Link>Remote Work</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Distance Learning</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Engineering</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Financial Services</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Sales</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>IT</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Marketing</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Customer Support</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Human Resources</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Project Management</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Media</Link>
+                    </LinkWrapper>
+                  </Dropdown>
+                ) : null}
               </NavItem>
               <NavItem>
-                <Text>Resources</Text>
+                <motion.div
+                  onHoverStart={() =>
+                    setHovered({ ...hovered, resources: true })
+                  }
+                  onHoverEnd={() =>
+                    setHovered({ ...hovered, resources: false })
+                  }>
+                  <Text>Resources</Text>
+                </motion.div>
+                {hovered.resources ? (
+                  <Dropdown>
+                    <LinkWrapper>
+                      <Link>Resources Library</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Slack Tips</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Blog</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Webinars</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Slack Certified Program</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Help Center</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>API</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>App Directory</Link>
+                    </LinkWrapper>
+                    <LinkWrapper>
+                      <Link>Download</Link>
+                    </LinkWrapper>
+                  </Dropdown>
+                ) : null}
               </NavItem>
               <NavItem>
                 <Text>Enterprise</Text>
@@ -80,8 +193,14 @@ const Nav = styled.div`
   }
 `;
 
-const NavItem = styled.div`
+const NavItem = styled(motion.div)`
+  position: relative;
+  border: none;
+  background-color: transparent;
   padding: 15px;
+  font-size: 16px;
+  font-family: 'CircularStd-Book';
+  cursor: pointer;
 `;
 
 const LaunchButton = styled.button`
@@ -99,6 +218,25 @@ const LaunchButton = styled.button`
   @media ${device.lg} {
     display: block;
   }
+`;
+
+const Dropdown = styled.div`
+  width: 250px;
+  background-color: white;
+  box-shadow: 0 0 2rem rgba(0, 0, 0, 0.1);
+  position: absolute;
+  top: 50px;
+  padding: 13px 20px;
+  border-radius: 5px;
+`;
+
+const LinkWrapper = styled.div`
+  padding: 8px 0;
+  display: flex;
+`;
+
+const Link = styled.a`
+  text-transform: none;
 `;
 
 const HamburgerWrapper = styled.div`
