c567f987274d4ecc76a64d11c5f4796876482527
Ken <a01056715@gmail.com>
2020-05-05 20:08:27 -0700

    finished footer sections responsiveness

--- client/src/components/Landing/Footer.jsx
+++ client/src/components/Landing/Footer.jsx
@@ -23,158 +23,160 @@ import * as b from '../../styles/blocks';
 export const Footer = () => {
   return (
     <Wrapper>
-      <Container>
-        <ContentWrapper>
-          <LogoWrapper>
-            <FooterLogo height={54} width={54} />
-          </LogoWrapper>
-          <SectionsWrapper>
-            <Sections>
-              <Section>
-                <b.Box py={2}>
-                  <b.Text
-                    color='#1d1d1d'
-                    uppercase
-                    fontSize={14}
-                    fontFamily='CircularStd-Medium'>
-                    Product
-                  </b.Text>
-                </b.Box>
-                <Items>
-                  <Item>
-                    <Link>Features</Link>
-                  </Item>
-                  <Item>
-                    <Link>Enterprise</Link>
-                  </Item>
-                  <Item>
-                    <Link>Security</Link>
-                  </Item>
-                  <Item>
-                    <Link>Customer Stories</Link>
-                  </Item>
-                  <Item>
-                    <Link>Pricing</Link>
-                  </Item>
-                  <Item>
-                    <Link>Slack Demo</Link>
-                  </Item>
-                </Items>
-              </Section>
-              <Section>
-                <b.Box py={2}>
-                  <b.Text
-                    color='#1d1d1d'
-                    uppercase
-                    fontSize={14}
-                    fontFamily='CircularStd-Medium'>
-                    Slack for teams
-                  </b.Text>
-                </b.Box>
-                <Items>
-                  <Item>
-                    <Link>Engineering</Link>
-                  </Item>
-                  <Item>
-                    <Link>Financial Services</Link>
-                  </Item>
-                  <Item>
-                    <Link>Sales</Link>
-                  </Item>
-                  <Item>
-                    <Link>IT</Link>
-                  </Item>
-                  <Item>
-                    <Link>Marketing</Link>
-                  </Item>
-                  <Item>
-                    <Link>Customer Support</Link>
-                  </Item>
-                  <Item>
-                    <Link>Human Resources</Link>
-                  </Item>
-                  <Item>
-                    <Link>Project Management</Link>
-                  </Item>
-                  <Item>
-                    <Link>Media</Link>
-                  </Item>
-                </Items>
-              </Section>
-            </Sections>
-            <Sections>
-              <Section>
-                <b.Box py={2}>
-                  <b.Text
-                    color='#1d1d1d'
-                    uppercase
-                    fontSize={14}
-                    fontFamily='CircularStd-Medium'>
-                    Resources
-                  </b.Text>
-                </b.Box>
-                <Items>
-                  <Item>
-                    <Link>Slack Tips</Link>
-                  </Item>
-                  <Item>
-                    <Link>Blog</Link>
-                  </Item>
-                  <Item>
-                    <Link>Events</Link>
-                  </Item>
-                  <Item>
-                    <Link>Slack Certified Program</Link>
-                  </Item>
-                  <Item>
-                    <Link>Help Center</Link>
-                  </Item>
-                  <Item>
-                    <Link>API</Link>
-                  </Item>
-                  <Item>
-                    <Link>App Directory</Link>
-                  </Item>
-                  <Item>
-                    <Link>Download Slack</Link>
-                  </Item>
-                  <Item>
-                    <Link>Partners</Link>
-                  </Item>
-                </Items>
-              </Section>
-              <Section>
-                <b.Box py={2}>
-                  <b.Text
-                    color='#1d1d1d'
-                    uppercase
-                    fontSize={14}
-                    fontFamily='CircularStd-Medium'>
-                    Company
-                  </b.Text>
-                </b.Box>
-                <Items>
-                  <Item>
-                    <Link>About Us</Link>
-                  </Item>
-                  <Item>
-                    <Link>Leadership</Link>
-                  </Item>
-                  <Item>
-                    <Link>News</Link>
-                  </Item>
-                  <Item>
-                    <Link>Media Kit</Link>
-                  </Item>
-                  <Item>
-                    <Link>Careers</Link>
-                  </Item>
-                </Items>
-              </Section>
-            </Sections>
-          </SectionsWrapper>
-        </ContentWrapper>
-      </Container>
+      <ContentWrapper>
+        <b.Container>
+          <ContentContainer>
+            <LogoWrapper>
+              <FooterLogo height={54} width={54} />
+            </LogoWrapper>
+            <SectionsWrapper>
+              <Sections>
+                <Section>
+                  <b.Box py={2}>
+                    <b.Text
+                      color='#1d1d1d'
+                      uppercase
+                      fontSize={14}
+                      fontFamily='CircularStd-Medium'>
+                      Product
+                    </b.Text>
+                  </b.Box>
+                  <Items>
+                    <Item>
+                      <Link>Features</Link>
+                    </Item>
+                    <Item>
+                      <Link>Enterprise</Link>
+                    </Item>
+                    <Item>
+                      <Link>Security</Link>
+                    </Item>
+                    <Item>
+                      <Link>Customer Stories</Link>
+                    </Item>
+                    <Item>
+                      <Link>Pricing</Link>
+                    </Item>
+                    <Item>
+                      <Link>Slack Demo</Link>
+                    </Item>
+                  </Items>
+                </Section>
+                <Section>
+                  <b.Box py={2}>
+                    <b.Text
+                      color='#1d1d1d'
+                      uppercase
+                      fontSize={14}
+                      fontFamily='CircularStd-Medium'>
+                      Slack for teams
+                    </b.Text>
+                  </b.Box>
+                  <Items>
+                    <Item>
+                      <Link>Engineering</Link>
+                    </Item>
+                    <Item>
+                      <Link>Financial Services</Link>
+                    </Item>
+                    <Item>
+                      <Link>Sales</Link>
+                    </Item>
+                    <Item>
+                      <Link>IT</Link>
+                    </Item>
+                    <Item>
+                      <Link>Marketing</Link>
+                    </Item>
+                    <Item>
+                      <Link>Customer Support</Link>
+                    </Item>
+                    <Item>
+                      <Link>Human Resources</Link>
+                    </Item>
+                    <Item>
+                      <Link>Project Management</Link>
+                    </Item>
+                    <Item>
+                      <Link>Media</Link>
+                    </Item>
+                  </Items>
+                </Section>
+              </Sections>
+              <Sections>
+                <Section>
+                  <b.Box py={2}>
+                    <b.Text
+                      color='#1d1d1d'
+                      uppercase
+                      fontSize={14}
+                      fontFamily='CircularStd-Medium'>
+                      Resources
+                    </b.Text>
+                  </b.Box>
+                  <Items>
+                    <Item>
+                      <Link>Slack Tips</Link>
+                    </Item>
+                    <Item>
+                      <Link>Blog</Link>
+                    </Item>
+                    <Item>
+                      <Link>Events</Link>
+                    </Item>
+                    <Item>
+                      <Link>Slack Certified Program</Link>
+                    </Item>
+                    <Item>
+                      <Link>Help Center</Link>
+                    </Item>
+                    <Item>
+                      <Link>API</Link>
+                    </Item>
+                    <Item>
+                      <Link>App Directory</Link>
+                    </Item>
+                    <Item>
+                      <Link>Download Slack</Link>
+                    </Item>
+                    <Item>
+                      <Link>Partners</Link>
+                    </Item>
+                  </Items>
+                </Section>
+                <Section>
+                  <b.Box py={2}>
+                    <b.Text
+                      color='#1d1d1d'
+                      uppercase
+                      fontSize={14}
+                      fontFamily='CircularStd-Medium'>
+                      Company
+                    </b.Text>
+                  </b.Box>
+                  <Items>
+                    <Item>
+                      <Link>About Us</Link>
+                    </Item>
+                    <Item>
+                      <Link>Leadership</Link>
+                    </Item>
+                    <Item>
+                      <Link>News</Link>
+                    </Item>
+                    <Item>
+                      <Link>Media Kit</Link>
+                    </Item>
+                    <Item>
+                      <Link>Careers</Link>
+                    </Item>
+                  </Items>
+                </Section>
+              </Sections>
+            </SectionsWrapper>
+          </ContentContainer>
+        </b.Container>
+      </ContentWrapper>
       <b.Box py={4} backgroundColor='#F5F5F5'>
         <b.Container>
           <FooterContentWrapper>
@@ -240,47 +242,50 @@ export const Footer = () => {
 };
 
 const Wrapper = styled.footer`
-  padding: 50px 0 0 0;
-
-  @media ${landing.lg} {
-    padding-top: 80px;
-  }
-
   @media ${landing.xl} {
     /* padding-top: 160px; */
     height: calc(100vh - 130px);
+    max-height: 400px;
   }
-`;
-
-const Container = styled(b.Container)`
-  padding-bottom: 50px;
 `;
 
 const LogoWrapper = styled(b.Box)`
   width: 100%;
-
-  @media ${landing.lg} {
+  margin-bottom: 30px;
+  @media ${landing.md} {
     width: 35%;
+    margin-bottom: 0;
   }
 `;
 
-const ContentWrapper = styled(b.Flex)`
-  display: block;
-  @media ${landing.lg} {
-    display: flex;
+const ContentWrapper = styled(b.Box)`
+  padding: 40px 0;
+  @media ${landing.md} {
+    padding: 70px 0;
+  }
+`;
+
+const ContentContainer = styled(b.Flex)`
+  flex-direction: column;
+  @media ${landing.md} {
+    flex-direction: row;
   }
 `;
 
 const SectionsWrapper = styled(b.Flex)`
   width: 100%;
-
-  @media ${landing.lg} {
+  flex-direction: column;
+  @media ${landing.md} {
+    flex-direction: row;
     width: 65%;
   }
 `;
 
 const Sections = styled(b.Flex)`
-  @media ${landing.lg} {
+  width: 100%;
+
+  @media ${landing.sm} {
+    flex-direction: column;
     width: 50%;
   }
 `;
