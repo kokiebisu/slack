99f15acd76bf62560cc81a1f67fe4c5e2d588230
Ken <a01056715@gmail.com>
2020-05-23 23:00:09 -0700

    changes color when sidebar option is selected && routes to necessary sidebar content

--- client/src/components/Home/Sidebar/Option.jsx
+++ client/src/components/Home/Sidebar/Option.jsx
@@ -1,34 +1,120 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 import { useHistory } from 'react-router-dom';
 
 import { Flex, Box, Text, Button } from '../../../styles/blocks';
 
-export const Option = ({ name, children, select, location }) => {
+import {
+  Threads,
+  MentionReaction,
+  People,
+  Drafts,
+  SavedItems,
+  Apps,
+  Files,
+  ShowLess,
+} from '../../../assets/svg/SidebarOptions';
+
+export const Option = ({ name }) => {
   const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
   const history = useHistory();
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClick(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setClicked(false);
+          return;
+        }
+        setClicked(true);
+      }
+
+      document.addEventListener('mousedown', handleClick);
+      return () => {
+        document.removeEventListener('mousedown', handleClick);
+      };
+    }, [ref]);
+  }
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
 
   return (
     <Wrapper
-      className={hovered ? `hovered` : ``}
+      ref={wrapperRef}
+      className={clicked ? `clicked` : hovered ? `hovered` : ``}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      onClick={() => history.push(`/client/${name}`)}>
+      onClick={() => {
+        history.push(`/client/${name}`);
+      }}>
       <Flex>
-        <Icon style={{ top: 1 }}>{children}</Icon>
+        <Icon style={{ top: 1 }}>{renderOption(name, clicked)}</Icon>
         <TextWrapper>
-          <Text
-            style={{ textTransform: 'capitalize' }}
+          <Title
+            className={clicked ? `clicked` : ``}
             fontFamily='Segoe'
-            color='#CFC3CF'
             fontSize={15}>
             {name}
-          </Text>
+          </Title>
         </TextWrapper>
       </Flex>
     </Wrapper>
   );
+};
+
+const renderOption = (option, clicked) => {
+  switch (option) {
+    case 'threads':
+      return (
+        <Threads width={15} height={15} color={clicked ? 'white' : '#CFC3CF'} />
+      );
+    case 'mention & reaction':
+      return (
+        <MentionReaction
+          width={15}
+          height={15}
+          color={clicked ? 'white' : '#CFC3CF'}
+        />
+      );
+    case 'drafts':
+      return (
+        <Drafts width={15} height={15} color={clicked ? 'white' : '#CFC3CF'} />
+      );
+    case 'saved items':
+      return (
+        <SavedItems
+          width={15}
+          height={15}
+          color={clicked ? 'white' : '#CFC3CF'}
+        />
+      );
+    case 'people':
+      return (
+        <People width={15} height={15} color={clicked ? 'white' : '#CFC3CF'} />
+      );
+    case 'apps':
+      return (
+        <Apps width={15} height={15} color={clicked ? 'white' : '#CFC3CF'} />
+      );
+    case 'files':
+      return (
+        <Files width={15} height={15} color={clicked ? 'white' : '#CFC3CF'} />
+      );
+    case 'show less':
+      return (
+        <ShowLess
+          width={15}
+          height={15}
+          color={clicked ? 'white' : '#CFC3CF'}
+        />
+      );
+  }
 };
 
 const Wrapper = styled(Button)`
@@ -41,7 +127,7 @@ const Wrapper = styled(Button)`
   }
 
   &.clicked {
-    background: red;
+    background: #1264a3;
   }
 `;
 
@@ -52,4 +138,13 @@ const Icon = styled(Box)`
 const TextWrapper = styled(Box)`
   position: relative;
   top: 2px;
+`;
+
+const Title = styled(Text)`
+  color: #cfc3cf;
+  text-transform: capitalize;
+
+  &.clicked {
+    color: white !important;
+  }
 `;
--- client/src/components/Home/Sidebar/Options.jsx
+++ client/src/components/Home/Sidebar/Options.jsx
@@ -4,17 +4,6 @@ import styled from 'styled-components';
 import { Box } from '../../../styles/blocks';
 import { Option } from './Option';
 
-import {
-  Threads,
-  MentionReaction,
-  People,
-  Drafts,
-  SavedItems,
-  Apps,
-  Files,
-  ShowLess,
-} from '../../../assets/svg/SidebarOptions';
-
 export const Options = () => {
   const defaultState = {
     threads: false,
@@ -28,41 +17,17 @@ export const Options = () => {
 
   const [selected, setSelected] = useState(defaultState);
 
-  const onSelect = (select) => {
-    setSelected(defaultState);
-    setSelected({
-      ...selected,
-      [select]: true,
-    });
-  };
-
   return (
     <OptionsWrapper>
       <Container>
-        <Option name='threads'>
-          <Threads width={15} height={15} color='#CFC3CF' />
-        </Option>
-        <Option name='mention & reaction'>
-          <MentionReaction width={15} height={15} color='#CFC3CF' />
-        </Option>
-        <Option name='drafts'>
-          <Drafts width={15} height={15} color='#CFC3CF' />
-        </Option>
-        <Option name='saved items' select={onSelect}>
-          <SavedItems width={15} height={15} color='#CFC3CF' />
-        </Option>
-        <Option name='people' select={onSelect}>
-          <People width={15} height={15} color='#CFC3CF' />
-        </Option>
-        <Option name='apps' select={onSelect}>
-          <Apps width={15} height={15} color='#CFC3CF' />
-        </Option>
-        <Option name='files' select={onSelect}>
-          <Files width={15} height={15} color='#CFC3CF' />
-        </Option>
-        <Option name='show less'>
-          <ShowLess width={15} height={15} color='#CFC3CF' />
-        </Option>
+        <Option name='threads' />
+        <Option name='mention & reaction' />
+        <Option name='drafts' />
+        <Option name='saved items' />
+        <Option name='people' />
+        <Option name='apps' />
+        <Option name='files' />
+        <Option name='show less' />
       </Container>
     </OptionsWrapper>
   );
