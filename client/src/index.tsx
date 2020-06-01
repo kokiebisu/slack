3ea5c4513d8f50e6e1daa0ba79ba7c896addf0b5
Ken <a01056715@gmail.com>
2020-06-01 15:59:08 -0700

    enabled sending refresh token when token expires

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,26 +7,104 @@ import { GlobalFonts } from './styles/globalFonts';
 import { GlobalStyles } from './styles/globalStyles';
 
 // Apollo
-import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { getAccessToken } from './global/token';
+import { setAccessToken, getAccessToken } from './global/token';
 // import { getAccessToken } from './global/token';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink, Observable } from 'apollo-link';
+
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
+import JwtDecode from 'jwt-decode';
+
+const cache = new InMemoryCache({});
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable((observer) => {
+      let handle: any;
+      Promise.resolve(operation)
+        .then((operation) => {
+          const accessToken = getAccessToken();
+          if (accessToken) {
+            operation.setContext({
+              headers: {
+                authorization: `bearer ${accessToken}`,
+              },
+            });
+          }
+        })
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer),
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
+
+// Passes through this on every graphql query
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  credentials: 'include',
-  cache: new InMemoryCache(),
-  request: (operation) => {
-    const accessToken = getAccessToken();
-    if (accessToken) {
-      operation.setContext({
-        headers: {
-          authorization: `bearer ${accessToken}`,
-        },
-      });
-    }
-  },
+  link: ApolloLink.from([
+    // Requests for a refresh token if the token is not valid anymore
+    new TokenRefreshLink({
+      // Checks whether if the token is still valid
+      isTokenValidOrUndefined: () => {
+        const token = getAccessToken();
+
+        if (!token) {
+          return true;
+        }
+
+        try {
+          // The token may still be expired
+          const { exp } = JwtDecode(token);
+          if (Date.now() >= exp * 1000) {
+            return false;
+          } else {
+            return true;
+          }
+        } catch {
+          return false;
+        }
+      },
+      fetchAccessToken: () => {
+        return fetch('http://localhost:4000/refresh_token', {
+          method: 'POST',
+          credentials: 'include',
+        });
+      },
+      handleFetch: (accessToken) => {
+        setAccessToken(accessToken);
+      },
+      handleError: (err) => {
+        // full control over handling token fetch Error
+        console.warn('Your refresh token is invalid. Try to relogin');
+        console.error(err);
+
+        // // your custom action here
+        // user.logout();
+      },
+    }) as any,
+    onError(({ graphQLErrors, networkError }) => {
+      console.log(networkError);
+    }),
+    requestLink,
+    new HttpLink({
+      uri: 'http://localhost:4000/graphql',
+      credentials: 'include',
+    }),
+  ]),
+  cache,
 });
 
 ReactDOM.render(
