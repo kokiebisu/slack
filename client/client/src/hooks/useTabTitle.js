da9bb208ef07f8be457c8aaf793943cb1b0e48e9
Ken <a01056715@gmail.com>
2020-06-01 14:07:05 -0700

    fixing redundant code

--- client/client/src/hooks/useTabTitle.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-
-export const useTabTitle = (section, group) => {
-  useEffect(() => {
-    document.title = `Slack | Threads | ${group}`;
-  }, []);
-};
--- client/server/src/middleware/logger.ts
+++
@@ -1,8 +0,0 @@
-import { MiddlewareFn } from 'type-graphql';
-import { Context } from '../types/context';
-
-export const logger: MiddlewareFn<Context> = ({ context }, next) => {
-  console.log(context);
-
-  return next();
-};
