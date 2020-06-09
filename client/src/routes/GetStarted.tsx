0c8c84174848d0f7d53784228ba2efd5a8e35664
Ken <a01056715@gmail.com>
2020-06-09 12:57:28 -0700

    created confirmEmail basic layout

--- client/src/routes/GetStarted.tsx
+++ client/src/routes/GetStarted.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
+// Pages
 import { GetStarted } from '../components/getstarted/layout';
 import { GetStartedFind } from '../components/getstartedFind/layout';
 import { GetStartedCreate } from '../components/getstartedCreate/layout';
+import { ConfirmEmail } from '../components/confirmEmail/layout';
 
 export const GetStartedRoutes: React.SFC = () => {
   const match = useRouteMatch();
@@ -13,6 +15,7 @@ export const GetStartedRoutes: React.SFC = () => {
         <Route exact path={match.url + '/'} component={GetStarted} />
         <Route path={match.url + '/find'} component={GetStartedFind} />
         <Route path={match.url + '/create'} component={GetStartedCreate} />
+        <Route path={match.url + '/check'} component={ConfirmEmail} />
       </Switch>
     </>
   );
