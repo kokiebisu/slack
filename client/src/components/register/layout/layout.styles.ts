09a333fded33a9fc04a58c8f9219a58e6b44a68d
Ken <a01056715@gmail.com>
2020-06-08 17:22:09 -0700

    refactored code for workspace

--- client/src/components/register/layout/layout.styles.ts
+++ client/src/components/register/layout/layout.styles.ts
@@ -1,37 +1,6 @@
 import styled from 'styled-components';
 import * as b from '../../../styles/blocks';
 
-// Animations
-export const DotContainterVariants = {
-  start: {
-    transition: {
-      staggerChildren: 0.3,
-    },
-  },
-  end: {
-    transition: {
-      staggerChildren: 0.3,
-    },
-  },
-};
-
-export const DotVariants = {
-  start: {
-    y: 0,
-  },
-  end: {
-    y: 5,
-  },
-};
-
-export const DotTransition = {
-  duration: 0.5,
-  yoyo: Infinity,
-  ease: 'easeInOut',
-};
-
-export const DotWrapper = styled(b.Box)``;
-
 export const confirmVariants = {
   loading: {
     x: -10,
@@ -44,33 +13,6 @@ export const confirmVariants = {
 export const Wrapper = styled(b.Box)`
   max-width: 768px;
   width: 100%;
-`;
-
-export const ConfirmButton = styled(b.Button)`
-  width: 370px;
-  border-radius: 5px;
-  padding: 13px 0;
-  text-align: center;
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.primary__light};
-  }
-`;
-
-export const DotContainer = styled(b.Box)`
-  position: relative;
-  bottom: 2px;
-  display: flex;
-  align-items: center;
-`;
-
-export const Dot = styled(b.Box)`
-  background-color: white;
-  border-radius: 50%;
-  width: 6px;
-  height: 6px;
-  margin: 0 2px;
 `;
 
 export const IconWrapper = styled(b.Box)`
---
+++ client/src/routes/Workspace.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+
+// Routing
+import {
+  Route,
+  Switch,
+  useRouteMatch,
+  RouteComponentProps,
+} from 'react-router-dom';
+import { StaticContext } from 'react-router';
+
+// Pages
+import { ThreadsPage } from '../components/workspace/content/threads';
+import { MentionPage } from '../components/workspace/content/mention';
+import { DraftPage } from '../components/workspace/content/draft';
+import { SavedPage } from '../components/workspace/content/saved';
+
+import { Workspace } from '../components/workspace/layout';
+
+type Props = RouteComponentProps<
+  {},
+  StaticContext,
+  { from: { pathname: string } }
+>;
+
+export const ClientRoutes: React.FC<Props> = () => {
+  return (
+    <>
+      <Switch>
+        <Workspace route={<WorkspaceRoute />} />
+      </Switch>
+    </>
+  );
+};
+
+export const WorkspaceRoute: React.FC<{}> = () => {
+  const match = useRouteMatch();
+  return (
+    <>
+      <Route path={match.url + '/saved-page'}>
+        <SavedPage />
+      </Route>
+      <Route path={match.url + '/threads'}>
+        <ThreadsPage />
+      </Route>
+      <Route path={match.url + '/drafts'}>
+        <DraftPage />
+      </Route>
+      <Route path={match.url + '/activity-page'}>
+        <MentionPage />
+      </Route>
+    </>
+  );
+};
