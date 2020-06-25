a2c03de764989a8f8d3c7e0733f0a83cf77de82e
Ken <a01056715@gmail.com>
2020-06-24 22:29:33 -0700

    enabled displaying username in section

--- client/src/components/workspace/content/directmessage/index.tsx
+++ client/src/components/workspace/content/directmessage/index.tsx
@@ -4,15 +4,18 @@ import { useParams } from 'react-router-dom';
 import { ClientContentLayout } from '../layout';
 import { Options } from '../../../shared/components/options';
 
+// Query
+import { useUserQuery } from '../../../../generated/graphql';
+
 interface Props {}
 
 export const DirectMessagePage: React.FC<Props> = () => {
   const { userId } = useParams();
-
+  const { data: userData } = useUserQuery({ variables: { userId } });
   return (
     <>
       <ClientContentLayout
-        section={`${userId}`}
+        section={`${userData?.user.user?.fullname}`}
         content={<div>content</div>}
         options={<Options />}
       />
