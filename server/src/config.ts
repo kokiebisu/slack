133965a281a5bc03ebca7cf90a2d4a804fd13bb4
Ken <a01056715@gmail.com>
2020-06-30 00:22:29 -0700

    created config file for server

---
+++ server/src/config.ts
@@ -0,0 +1,8 @@
+var config: any = {};
+
+config.REDIS_PORT =
+  process.env.NODE_ENV === 'production' ? process.env.REDIS_PORT : 6379;
+config.REDIS_HOST =
+  process.env.NODE_ENV === 'production' ? process.env.REDIS_HOST : 'localhost';
+
+export { config };
--- server/src/redis.ts
+++ server/src/redis.ts
@@ -1,3 +1,7 @@
 import Redis from 'ioredis';
+import { config } from './config';
 
-export const redis = new Redis();
+export const redis = new Redis({
+  host: config.REDIS_HOST,
+  port: config.REDIS_PORT,
+});
