b1ecf53df3505b33121f685c88b88cef88ba290c
Ken <a01056715@gmail.com>
2020-07-10 23:09:38 -0700

    fixed config for server

---
+++ server/config.ts
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+interface ConfigProps {
+  redisHost: string;
+  redisPort: number;
+  pgUser: string;
+  pgHost: string;
+  pgDatabase: string;
+  pgPassword: string;
+  pgPort: number;
+}
+
+export const config: ConfigProps = {
+  redisHost: process.env.REDIS_HOST!,
+  redisPort: parseInt(process.env.REDIS_PORT!),
+  pgUser: process.env.POSTGRES_USER!,
+  pgHost: process.env.POSTGRES_HOST!,
+  pgDatabase: process.env.POSTGRES_DB!,
+  pgPassword: process.env.POSTGRES_PASSWORD!,
+  pgPort: parseInt(process.env.POSTGRES_PORT!),
+};
