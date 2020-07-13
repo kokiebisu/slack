1a15b1e9e5db1046c65cb9ee5b862563396006a5
Ken <a01056715@gmail.com>
2020-07-12 18:59:41 -0700

    removed build

--- server/dist/src/constants/redisPrefixes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.forgotPasswordPrefix = exports.confirmationPrefix = void 0;
-exports.confirmationPrefix = `user-confirmation: `;
-exports.forgotPasswordPrefix = `forgot-password: `;
-//# sourceMappingURL=redisPrefixes.js.map
