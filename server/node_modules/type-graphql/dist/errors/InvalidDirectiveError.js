c5451ab52039247e77c4a2f011523bf5e1636a52
Ken <a01056715@gmail.com>
2020-07-14 17:17:56 -0700

    removed node modules

--- server/node_modules/type-graphql/dist/errors/InvalidDirectiveError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class InvalidDirectiveError extends Error {
-    constructor(msg) {
-        super(msg);
-        Object.setPrototypeOf(this, new.target.prototype);
-    }
-}
-exports.InvalidDirectiveError = InvalidDirectiveError;
