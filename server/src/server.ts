56571378ef0ff30e4e1d30ea14315e2ac9b06d62
Ken <a01056715@gmail.com>
2020-07-15 18:08:40 -0700

    modified headers for server

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,20 +8,24 @@ import session from 'express-session';
 import cors from 'cors';
 import connectRedis from 'connect-redis';
 import cookieParser from 'cookie-parser';
-// import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { redis } from './redis';
 import { router as tokenRouter } from './routes/tokenRoutes';
 
 const PORT = process.env.PORT || 4000;
 
-// const allowCrossDomain = (_: Request, res: Response, next: NextFunction) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3050');
-//   res.setHeader('Access-Control-Allow-Headers', 'http://localhost:3050');
-//   res.setHeader('Access-Control-Allow-Origin', 'http://34.105.56.225:80');
-//   res.setHeader('Access-Control-Allow-Headers', 'http://34.105.56.225:80');
-//   res.setHeader('Access-Control-Allow-Credentials', 'true');
-//   next();
-// };
+const allowCrossDomain = (_: Request, res: Response, next: NextFunction) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://slack-green.vercel.app'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'https://slack-green.vercel.app'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+};
 
 (async () => {
   let path = __dirname + '/graphql/**/*.js' || __dirname + '/graphql/**/*.ts';
@@ -43,10 +47,10 @@ const PORT = process.env.PORT || 4000;
   app.use(
     cors({
       credentials: true,
-      origin: 'http://localhost:3050',
+      origin: 'https://slack-green.vercel.app',
     })
   );
-
+  app.use(allowCrossDomain);
   app.use(cookieParser());
 
   const apolloServer = new ApolloServer({
