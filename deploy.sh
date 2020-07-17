e8b1cc5c2cc04161d3b9c12c184e129d4b26b2b6
Ken <a01056715@gmail.com>
2020-07-16 23:04:50 -0700

    configured deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t kokiebisu/slack-client:latest -f ./client/Dockerfile ./client
-docker build -t kokiebisu/slack-server:latest -f ./server/Dockerfile ./server
+docker build -t kokiebisu/slack-client -f ./client/Dockerfile ./client
+docker build -t kokiebisu/slack-server -f ./server/Dockerfile ./server
 
-docker push kokiebisu/slack-client:latest
-docker push kokiebisu/slack-server:latest
+docker push kokiebisu/slack-client
+docker push kokiebisu/slack-server
 
