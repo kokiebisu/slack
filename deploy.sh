9ceeef20a7288d52674ca0888ab20f5092195634
Ken <a01056715@gmail.com>
2020-07-16 22:31:07 -0700

    fixed shell script error

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
-docker build -t kokiebisu/slack-client:latest -t kokiebisu/slack-client:$SHA -f ./client/Dockerfile ./client
-docker build -t kokiebisu/slack-server:latest -t kokiebisu/slack-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kokiebisu/slack-client:latest -f ./client/Dockerfile ./client
+docker build -t kokiebisu/slack-server:latest -f ./server/Dockerfile ./server
 
 docker push kokiebisu/slack-client:latest
 docker push kokiebisu/slack-server:latest
 
-docker push kokiebisu/slack-client:$SHA
-docker push kokiebisu/slack-server:$SHA
