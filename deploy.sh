45b38ae8f1e8d86837e20bce527744a705d590b7
Ken <a01056715@gmail.com>
2020-07-20 11:39:22 -0700

    added new docker push for proxy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t kokiebisu/slack-client -f ./client/Dockerfile ./client
 docker build -t kokiebisu/slack-server -f ./server/Dockerfile ./server
+docker build -t kokiebisu/slack-proxy -f ./proxy/Dockerfile ./proxy
 
 docker push kokiebisu/slack-client
 docker push kokiebisu/slack-server
-
+docker push kokiebisu/slack-proxy
