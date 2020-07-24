dfb3ff8608a12cbcebcd9ad3938873320684b096
Kenichi Okiebisu <a01056715@gmail.com>
2020-07-24 01:38:10 -0700

    added svgs to olanding modal

--- client/src/components/Landing/Modal/layout/index.styles.tsx
+++ client/src/components/Landing/Modal/layout/index.styles.tsx
@@ -1,4 +1,40 @@
 import * as b from '../../../../styles/blocks';
 import styled from 'styled-components';
 
-export const Wrapper = styled(b.Box)``;
+export const Wrapper = styled(b.Box)`
+  .header {
+    padding: 25px;
+    margin-bottom: 7px;
+  }
+  .content {
+    padding-right: 20px;
+  }
+`;
+
+export const IconWrapper = styled(b.Box)`
+  &.logo {
+    svg {
+      width: 100px;
+      height: auto;
+    }
+  }
+  &.arrow {
+    svg {
+      width: 10px;
+    }
+  }
+`;
+
+export const IconButtonWrapper = styled(b.Button)`
+  svg {
+    width: 14px;
+  }
+`;
+
+export const Option = styled(b.Box)`
+  padding: 17px 25px;
+  p {
+    font-family: 'SlackLato-Regular';
+    font-size: 17px;
+  }
+`;
--- client/src/components/Landing/Modal/layout/index.tsx
+++ client/src/components/Landing/Modal/layout/index.tsx
@@ -1,34 +1,56 @@
 import React from 'react';
-import { Wrapper } from './index.styles';
+import {
+  Wrapper,
+  IconWrapper,
+  IconButtonWrapper,
+  Option,
+} from './index.styles';
 import * as b from '../../../../styles/blocks';
 import { Close } from '../../../../assets/svg';
+import { NameLogo } from '../../../../assets/svg/Logo';
+import { ChevronRight } from '../../../../assets/svg/Arrows';
+import { useToggleDispatch } from '../../../../context/toggle-context';
 
 export const LandingModal = () => {
+  const dispatchToggle = useToggleDispatch();
+
   const options = [
-    { name: 'Why Slack?' },
-    { name: 'Solutions' },
-    { name: 'Resources' },
+    { name: 'Why Slack?', sub: [] },
+    { name: 'Solutions', sub: [] },
+    { name: 'Resources', sub: [] },
     { name: 'Enterprise' },
     { name: 'Pricing' },
   ];
   return (
     <Wrapper>
       <b.Box className='header'>
-        <b.Flex>
-          <b.Box>
+        <b.Flex justifyContent='space-between' alignItems='center'>
+          <IconWrapper className='logo'>
             <NameLogo />
-          </b.Box>
-          <b.Box>
+          </IconWrapper>
+          <IconButtonWrapper
+            onClick={() => dispatchToggle({ type: 'toggle_landing' })}>
             <Close />
-          </b.Box>
+          </IconButtonWrapper>
         </b.Flex>
       </b.Box>
       <b.Box className='content'>
         {options.map((option) => {
           return (
-            <b.Box>
-              <b.Text>{option.name}</b.Text>
-            </b.Box>
+            <Option>
+              <b.Flex justifyContent='space-between' alignItems='center'>
+                <b.Box>
+                  <b.Text>{option.name}</b.Text>
+                </b.Box>
+                {option.sub && (
+                  <b.Box>
+                    <IconWrapper className='arrow'>
+                      <ChevronRight />
+                    </IconWrapper>
+                  </b.Box>
+                )}
+              </b.Flex>
+            </Option>
           );
         })}
       </b.Box>
