9d931161542ff7dc31dbb00c717e2596dfbcba44
Kenichi Okiebisu <a01056715@gmail.com>
2020-07-24 01:03:17 -0700

    added basic structure for landing modal component

---
+++ client/src/components/Landing/Modal/layout/index.styles.tsx
@@ -0,0 +1,4 @@
+import * as b from '../../../../styles/blocks';
+import styled from 'styled-components';
+
+export const Wrapper = styled(b.Box)``;
--- client/src/components/Landing/Modal/layout/index.tsx
+++ client/src/components/Landing/Modal/layout/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import { Wrapper } from './index.styles';
+import * as b from '../../../../styles/blocks';
+import { Close } from '../../../../assets/svg';
 
 export const LandingModal = () => {
-  return <div>landing modal</div>;
+  const options = [
+    { name: 'Why Slack?' },
+    { name: 'Solutions' },
+    { name: 'Resources' },
+    { name: 'Enterprise' },
+    { name: 'Pricing' },
+  ];
+  return (
+    <Wrapper>
+      <b.Box className='header'>
+        <b.Flex>
+          <b.Box>
+            <NameLogo />
+          </b.Box>
+          <b.Box>
+            <Close />
+          </b.Box>
+        </b.Flex>
+      </b.Box>
+      <b.Box className='content'>
+        {options.map((option) => {
+          return (
+            <b.Box>
+              <b.Text>{option.name}</b.Text>
+            </b.Box>
+          );
+        })}
+      </b.Box>
+    </Wrapper>
+  );
 };
