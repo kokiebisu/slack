c7ac4778379d90d8b0ce9987703189b73ac7f2ce
Kenichi Okiebisu <a01056715@gmail.com>
2020-07-29 00:20:24 -0700

    added content for top inner

--- client/src/components/Landing/BetterSoftware/Layout/index.styles.ts
+++ client/src/components/Landing/BetterSoftware/Layout/index.styles.ts
@@ -1,9 +1,85 @@
 import styled from 'styled-components';
-
 import * as b from '../../../../styles/blocks';
 
-export const Wrapper = styled(b.Box)`
+// Breakpoints;
+import { size } from '../../../../styles/sizes';
+
+export const Wrapper = styled.section`
   padding-top: 100px;
   padding-bottom: 30px;
+
   background-color: ${({ theme }) => theme.colors.white__darker};
+`;
+
+export const InnerTop = styled(b.Box)`
+  @media ${size.md} {
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+  }
+
+  & > div:first-child {
+    padding-left: 50px;
+  }
+`;
+
+export const LearnMore = styled(b.Box)`
+  width: 100%;
+  & > a {
+    font-size: 18px;
+    color: ${({ theme }) => theme.colors.blue};
+  }
+
+  & div {
+    justify-content: center;
+    @media ${size.lg} {
+      justify-content: flex-start;
+    }
+  }
+`;
+
+export const ImageWrapper = styled(b.Box)`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+
+  @media ${size.md} {
+    flex-grow: 1;
+    bottom: 50px;
+  }
+`;
+
+export const Title = styled(b.Box)`
+  & p {
+    font-family: 'SlackLato-Light';
+    color: ${({ theme }) => theme.colors.black__light};
+    font-size: 52px;
+    line-height: 1.2;
+    font-family: 'Larsseit-Bold';
+  }
+`;
+
+export const Description = styled(b.Text)`
+  font-size: 18px;
+  line-height: 1.4;
+  text-align: center;
+
+  @media ${size.md} {
+    text-align: left;
+  }
+`;
+
+export const CheckList = styled(b.Box)`
+  & ul {
+    padding: 0;
+    list-style: none;
+  }
+`;
+
+export const ColumnWrapper = styled(b.Box)`
+  padding-bottom: 62px;
+  @media ${size.sm} {
+    padding-top: 50px;
+  }
 `;
--- client/src/components/Landing/BetterSoftware/Layout/index.tsx
+++ client/src/components/Landing/BetterSoftware/Layout/index.tsx
@@ -1,12 +1,77 @@
 import React from 'react';
-import { Wrapper } from './index.styles';
+import {
+  Wrapper,
+  InnerTop,
+  Title,
+  Description,
+  CheckList,
+  LearnMore,
+  ImageWrapper,
+} from './index.styles';
 
 import * as b from '../../../../styles/blocks';
+
+import integrations from '../../../../assets/img/slack-integrations.png';
 
 export const BetterSoftware = () => {
   return (
     <Wrapper>
-      <b.Container>hello</b.Container>
+      <b.Container>
+        <InnerTop>
+          <b.Box>
+            <b.Flex justifyContent='center'>
+              <ImageWrapper>
+                <img src={integrations} />
+              </ImageWrapper>
+            </b.Flex>
+          </b.Box>
+          <b.Box>
+            <b.Flex flexDirection='column' alignItems='center'>
+              <Title>
+                <b.Text>Slack makes other software better</b.Text>
+              </Title>
+              <b.Box my={3}>
+                <Description>
+                  Make your tools more accessible and useful by connecting them
+                  all in one place.
+                </Description>
+              </b.Box>
+              <CheckList>
+                <ul>
+                  <li>
+                    <b.Flex>
+                      <b.Box></b.Box>
+                      <b.Box>Connect over 2,200 ready-to-use Slack apps</b.Box>
+                    </b.Flex>
+                  </li>
+                  <li>
+                    <b.Flex>
+                      <b.Box></b.Box>
+                      <b.Box>Build your own tim-saving workflows</b.Box>
+                    </b.Flex>
+                  </li>
+                  <li>
+                    <b.Flex>
+                      <b.Box></b.Box>
+                      <b.Box>
+                        Integrate your internal tools with the Slack APIs
+                      </b.Box>
+                    </b.Flex>
+                  </li>
+                </ul>
+              </CheckList>
+              <LearnMore>
+                <b.Anchor>
+                  <b.Flex>
+                    <b.Text>Learn more about integrations</b.Text>
+                    <b.Text>&nbsp;→</b.Text>
+                  </b.Flex>
+                </b.Anchor>
+              </LearnMore>
+            </b.Flex>
+          </b.Box>
+        </InnerTop>
+      </b.Container>
     </Wrapper>
   );
 };
