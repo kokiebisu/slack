dd4ef94840d519e2b86012c96199f6f318408e8c
Kenichi Okiebisu <a01056715@gmail.com>
2020-07-31 17:55:21 -0700

    converted styles directory into absolute import

--- client/src/styles/globalFonts.ts
+++ client/src/styles/globalFonts.ts
@@ -1,15 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 // Fonts
-import CircularProBook from '../assets/fonts/CircularPro-Book.woff2';
-import CircularProBold from '../assets/fonts/CircularPro-Bold.woff2';
-import LarsseitBold from '../assets/fonts/Larsseit-Bold.woff2';
+import CircularProBook from 'assets/fonts/CircularPro-Book.woff2';
+import CircularProBold from 'assets/fonts/CircularPro-Bold.woff2';
+import LarsseitBold from 'assets/fonts/Larsseit-Bold.woff2';
 
-import SlackLatoLight from '../assets/fonts/SlackLato-Light.woff';
-import SlackLatoRegular from '../assets/fonts/SlackLato-Regular.woff';
-import SlackLatoBold from '../assets/fonts/SlackLato-Bold.woff';
-import SlackLatoBlack from '../assets/fonts/SlackLato-Black.woff2';
-import SlackIcons from '../assets/fonts/SlackIcons.woff2';
+import SlackLatoLight from 'assets/fonts/SlackLato-Light.woff';
+import SlackLatoRegular from 'assets/fonts/SlackLato-Regular.woff';
+import SlackLatoBold from 'assets/fonts/SlackLato-Bold.woff';
+import SlackLatoBlack from 'assets/fonts/SlackLato-Black.woff2';
+import SlackIcons from 'assets/fonts/SlackIcons.woff2';
 
 export const GlobalFonts = createGlobalStyle`
 
