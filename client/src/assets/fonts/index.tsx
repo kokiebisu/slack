6d072bfb942329e253ee89b2f8d1dae2ad848cfc
Kenichi Okiebisu <a01056715@gmail.com>
2020-07-31 22:25:33 -0700

    organized assets/fonts

---
+++ client/src/assets/fonts/index.tsx
@@ -0,0 +1,20 @@
+import CircularProBook from 'assets/fonts/CircularPro-Book.woff2';
+import CircularProBold from 'assets/fonts/CircularPro-Bold.woff2';
+import LarsseitBold from 'assets/fonts/Larsseit-Bold.woff2';
+
+import SlackLatoLight from 'assets/fonts/SlackLato-Light.woff';
+import SlackLatoRegular from 'assets/fonts/SlackLato-Regular.woff';
+import SlackLatoBold from 'assets/fonts/SlackLato-Bold.woff';
+import SlackLatoBlack from 'assets/fonts/SlackLato-Black.woff2';
+import SlackIcons from 'assets/fonts/SlackIcons.woff2';
+
+export {
+  CircularProBook,
+  CircularProBold,
+  LarsseitBold,
+  SlackLatoLight,
+  SlackLatoRegular,
+  SlackLatoBold,
+  SlackLatoBlack,
+  SlackIcons,
+};
