4940270c86ee2e628bab2ae68e42808731c8cda4
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-05 13:21:05 -0700

    recognizes enter key being pressed

--- client/src/components/Workspace/MessageBox/Editor.tsx
+++ client/src/components/Workspace/MessageBox/Editor.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import { useState } from 'react';
-import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
-import styled from 'styled-components';
+import {
+  Editor,
+  EditorState,
+  RichUtils,
+  getDefaultKeyBinding,
+  KeyBindingUtil,
+} from 'draft-js';
 import { Bold } from 'assets/svg';
 
 import { Wrapper } from 'styles/Workspace/MessageBox/Editor';
 
 export const MyEditor = () => {
+  const { hasCommandModifier } = KeyBindingUtil;
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
   const boldText = (e: any) => {
     // onMouseDown and e.preventDefault because editor losses focus if you use onClick
     e.preventDefault();
-
     let nextState = RichUtils.toggleInlineStyle(editorState, 'BOLD');
-
     setEditorState(nextState);
+  };
+
+  function keyBindingFn(e: any) {
+    const content = editorState.getCurrentContent();
+    if (e.keyCode === 13 && content.hasText()) {
+      console.log('entered');
+      return 'send';
+    }
+    return getDefaultKeyBinding(e);
+  }
+
+  const handleKeyCommand = (command: string) => {
+    let newState;
+    if (command === 'send') {
+      // send
+      newState = EditorState.createEmpty();
+    }
+    if (newState) {
+      setEditorState(newState);
+      return 'handled';
+    }
+    return 'not-handled';
   };
 
   return (
@@ -23,6 +49,8 @@ export const MyEditor = () => {
       <Editor
         placeholder='Message #general'
         editorState={editorState}
+        keyBindingFn={keyBindingFn}
+        handleKeyCommand={(command) => handleKeyCommand(command)}
         onChange={(editorState) => setEditorState(editorState)}
       />
       <div>
--- client/src/styles/Workspace/MessageBox/Editor.ts
+++ client/src/styles/Workspace/MessageBox/Editor.ts
@@ -5,29 +5,38 @@ export const Wrapper = styled(b.Box)`
   border: 1px solid black;
   border-radius: 4px;
   & > div:first-child {
-    padding: 10px 5px;
+    margin: 10px 0 10px 10px;
   }
   & > div:last-child {
     border-top: 1px solid #dddddd;
     background-color: #f8f8f8;
-    padding: 7px 5px;
+    padding: 5px 5px;
     border-radius: inherit;
   }
   button {
     border: none;
     background-color: transparent;
-    height: 21px;
-    width: 21px;
+    height: 25px;
+    width: 25px;
     cursor: pointer;
     svg {
-      height: 100%;
-      width: 100%;
+      height: 12px;
+      width: 12px;
       path {
         fill: black;
       }
     }
     &:hover {
       background: darkgray;
+    }
+    &:first-child {
+      border-right: 1px solid #dddddd;
+    }
+  }
+
+  div {
+    &.public-DraftStyleDefault-block {
+      right: 2px;
     }
   }
 `;
