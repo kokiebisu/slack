f6d8effbbcaed3c1c3936a5f561b3957379ffabe
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-23 15:56:00 -0700

    added globalDecorator to storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,19 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalFonts } from 'global/globalFonts';
+import { GlobalStyles } from 'global/globalStyles';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+const globalProvider = (Story, context) => {
+  return (
+    <ThemeProvider theme={main}>
+      <GlobalFonts />
+      <GlobalStyles />
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
+
+export const decorators = [globalProvider];
