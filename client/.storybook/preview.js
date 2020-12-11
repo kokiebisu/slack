06f158e80e663891f411cd1e4da055454af3b8d0
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-11 00:04:44 -0800

    fixed preview.js storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,25 @@
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { MemoryRouter } from "react-router-dom";
+import { ToggleProvider } from "../src/context/toggle-context";
+import { ThemeProvider } from "styled-components";
+import { client } from "../src/index";
+import { main } from "../src/global/colors";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ApolloProvider client={client}>
+      <MemoryRouter>
+        <ToggleProvider>
+          <ThemeProvider theme={main}>
+            <Story />
+          </ThemeProvider>
+        </ToggleProvider>
+      </MemoryRouter>
+    </ApolloProvider>
+  ),
+];
