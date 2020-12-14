34e7f88b84d96c1b751c07b8a8482eafb8a3b391
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-13 18:09:49 -0800

    refactored all buttons

--- client/src/components/atoms/button/button.transparent.tsx
+++ client/src/components/atoms/button/button.transparent.tsx
@@ -5,29 +5,26 @@ import styled from "styled-components";
 export const TransparentButton: React.FC<{
   title?: string;
 }> = ({ title = "Transparent Button" }) => {
+  const theme = useContext(ThemeContext);
   return (
-    <Wrapper>
-      <b.Text>{title}</b.Text>
+    <Wrapper
+      display="inline-block"
+      borderRadius={3}
+      px={4}
+      py={3}
+      backgroundColor="transparent"
+      border="none"
+      color={theme.colors.gray__dark}
+    >
+      <b.Text nowrap>{title}</b.Text>
     </Wrapper>
   );
 };
 
 const Wrapper = styled(b.Box)`
-  display: inline-block;
-  padding: 8px 13px;
-  border-radius: 3px;
-  color: ${({ theme }) => theme.colors.gray__dark};
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-
   &:hover {
     color: ${({ theme }) => theme.colors.blue};
     border-color: ${({ theme }) => theme.colors.gray};
     text-decoration: underline;
   }
-`;
-
-const H3 = styled(b.Text)`
-  white-space: nowrap;
 `;
